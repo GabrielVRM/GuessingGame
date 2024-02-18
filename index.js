a722e68d63ef8ed37c69b2e4cdd82705f8bd4926
GabrielVRM <gabriel.vieira2595@gmail.com>
2024-02-18 17:29:14 -0300

    refactor code

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-const screen1 = document.querySelector('.screen1');
-const screen2 = document.querySelector('.screen2');
-
-const rondomNumber = Math.round(Math.random() * 10);
+// variables
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+let rondomNumber = Math.round(Math.random() * 10);
 let xAttempts = 1;
+
+//events
+btnTry.addEventListener("click", handleClick);
+btnReset.addEventListener("click", handleReset);
+document.addEventListener("keydown", handleKeydown);
 
 //function callback
 function handleClick(event) {
   event.preventDefault();
-  const inputNumber = document.querySelector('#inputNumber');
+  const inputNumber = document.querySelector("#inputNumber");
+
+  if (Number(inputNumber.value) < 0 || Number(inputNumber.value) > 10) {
+    alert("numbers 0 to 10");
+  }
 
   if (Number(inputNumber.value) == rondomNumber) {
-    screen1.classList.toggle('hide');
-    screen2.classList.toggle('hide');
-    document.querySelector('screen2 h2').innerText(`you got it right: ${xAttempts}`);
-  } else {
-    alert('error');
+    toogleScreen();
+    document.querySelector(
+      ".screen2 h2"
+    ).innerText = `you got it right: ${xAttempts}`;
   }
+  inputNumber.value = "";
   xAttempts++;
 }
-//events
-const btnTry = document.querySelector('#btnTry');
-const btnReset = document.querySelector('#btnReset');
-
-btnTry.addEventListener('click', handleClick);
-btnReset.addEventListener('click', function () {});
+function handleReset() {
+  toogleScreen();
+  rondomNumber = Math.round(Math.random() * 10);
+  xAttempts = 1;
+}
+function handleKeydown(e) {
+  if (e.key == "Enter" && screen1.classList.contains("hide")) {
+    handleReset();
+  }
+}
+function toogleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
